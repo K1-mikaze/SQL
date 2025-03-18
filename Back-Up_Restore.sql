6c20261bd7b614b32b81fbc20d326625a52e0417
K1-mikaze <K1mikaze@proton.me>
2025-03-18 14:02:25 -0500

    Back-up & restore

---
+++ Back-Up_Restore.sql
@@ -0,0 +1,6 @@
+-- Para hacer un Back Up de la base de datos utilizamos el comando:
+mysqldump -B -u root -p (Nombre base de datos)> (direccion/de /memoria)
+
+Para Restaurar un back up de la base de datos es decir subir un back up a la base de datos:
+
+source (RutaDelArchivo);
